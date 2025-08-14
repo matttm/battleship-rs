d6d3764fe239c50c38bd9c0db3c197609d05b9db
matttm <matttmaloney@gmail.com>
2025-08-14 10:48:42 -0400

    adding settings to maker struct

--- battleship_client/src/match_maker.rs
+++ battleship_client/src/match_maker.rs
@@ -1,8 +1,13 @@
-pub struct MatchMaker {}
+pub struct MatchMaker {
+    settings: battleship_models::Settings,
+}
+
+use battleship_models;
 
 impl MatchMaker {
-    const settings = None();
     pub fn new() -> Self {
-        return MatchMaker {};
+        return MatchMaker {
+            settings: battleship_models::Settings { rows: 8, cols: 8 },
+        };
     }
 }
