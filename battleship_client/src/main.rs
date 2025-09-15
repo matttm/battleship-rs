7732222a7f247afc3ec44f143b41b61f824d4e05
matttm <matttmaloney@gmail.com>
2025-09-15 09:56:15 -0400

    cargo generating

--- battleship_client/src/main.rs
+++ battleship_client/src/main.rs
@@ -4,22 +4,34 @@ use env_logger::{Builder, Target};
 use futures_util::{SinkExt, StreamExt};
 use log::info;
 use tokio_tungstenite::tungstenite::Message;
+
+pub mod app;
+pub mod event;
+pub mod ui;
+
+use app::App;
+
 /// A WebSocket echo server
 #[tokio::main]
-async fn main() {
-    let args: Vec<String> = env::args().collect();
-    let url: String;
-    // Initialize env_logger to output to stdout with info level
-    let mut builder = Builder::from_default_env();
-    builder.target(Target::Stdout); // Set the output target to stdout
-    builder.filter_level(log::LevelFilter::Info); // Set the minimum log level to info
-    builder.init();
-    info!("Initializing BATTLESHIP");
-    url = args[1].to_string();
-    info!("Connecting to server");
-    let (mut socket, _) = tokio_tungstenite::connect_async(format!("ws://{url}"))
-        .await
-        .expect("Cannot connect to game server");
-    let (tx, rx) = socket.split();
-    info!("Connection established");
+async fn main() -> color_eyre::Result<()> {
+    // let args: Vec<String> = env::args().collect();
+    // let url: String;
+    // // Initialize env_logger to output to stdout with info level
+    // let mut builder = Builder::from_default_env();
+    // builder.target(Target::Stdout); // Set the output target to stdout
+    // builder.filter_level(log::LevelFilter::Info); // Set the minimum log level to info
+    // builder.init();
+    // info!("Initializing BATTLESHIP");
+    // url = args[1].to_string();
+    // info!("Connecting to server");
+    // let (mut socket, _) = tokio_tungstenite::connect_async(format!("ws://{url}"))
+    //     .await
+    //     .expect("Cannot connect to game server");
+    // let (tx, rx) = socket.split();
+    // info!("Connection established");
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal).await;
+    ratatui::restore();
+    result
 }
